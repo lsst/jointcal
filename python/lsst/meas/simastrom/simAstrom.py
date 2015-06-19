69a17ae31f0c17abbcc16e596c398d64a9ba1201
Dominique Boutigny <boutigny@in2p3.fr>
2015-06-19 14:23:59 -0700

    create basic simAstrom command line task able to retrieve a set of calexp and to run on the whole set as a single target

---
+++ python/lsst/meas/simastrom/simAstrom.py
@@ -0,0 +1,72 @@
+from __future__ import division, absolute_import
+#
+# LSST Data Management System
+# Copyright 2008, 2009, 2010, 2011, 2012 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+import numpy
+
+import lsst.pex.config as pexConfig
+import lsst.afw.image as afwImage
+import lsst.coadd.utils as coaddUtils
+import lsst.pipe.base as pipeBase
+
+from lsst.afw.fits import FitsError
+from lsst.pipe.tasks.selectImages import WcsSelectImagesTask, SelectStruct
+from lsst.coadd.utils import CoaddDataIdContainer
+from lsst.pipe.tasks.getRepositoryData import DataRefListRunner
+
+__all__ = ["SimAstromTask"]
+
+class SimAstromConfig(pexConfig.Config):
+    """Config for SimAstromTask
+    """
+
+# Keep this config parameter as a place holder
+    doWrite = pexConfig.Field(
+        doc = "persist SimAstrom output...",
+        dtype = bool,
+        default = True,
+    )
+
+class SimAstromTask(pipeBase.CmdLineTask):
+ 
+    ConfigClass = SimAstromConfig
+    RunnerClass = DataRefListRunner
+    _DefaultName = "simAstrom"
+    
+    def __init__(self, *args, **kwargs):
+        pipeBase.Task.__init__(self, *args, **kwargs)
+#        self.makeSubtask("select")
+        
+    @classmethod
+    def _makeArgumentParser(cls):
+        """Create an argument parser
+        """
+        parser = pipeBase.ArgumentParser(name=cls._DefaultName)
+
+        parser.add_id_argument("--id", "calexp", help="data ID, e.g. --selectId visit=6789 ccd=0..9")
+        return parser
+
+    @pipeBase.timeMethod
+    def run(self, ref):
+        
+        for r in ref :
+            print r.dataId
