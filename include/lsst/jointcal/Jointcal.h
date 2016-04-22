6b9d10d7ee5d6e44f5ff602e73d3ade1e0fcff94
Pierre Astier <pierre.astier@in2p3.fr>
2016-04-22 18:40:29 +0200

    added fields to the control struct

--- include/lsst/jointcal/Jointcal.h
+++ include/lsst/jointcal/Jointcal.h
@@ -9,7 +9,7 @@
 
 #include "lsst/pex/config.h"
 #include "lsst/afw/table/Source.h"
-#include "lsst/afw/image/Wcs.h"
+#include "lsst/afw/image/TanWcs.h"
 #include "lsst/afw/image/Calib.h"
 #include "lsst/afw/geom/Box.h"
 #include "lsst/daf/base/PropertySet.h"
@@ -19,10 +19,23 @@ namespace jointcal {
     
     struct JointcalControl {
         LSST_CONTROL_FIELD(sourceFluxField, std::string, "name of flux field in source catalog");
+      LSST_CONTROL_FIELD(linMatchCut, double, "max distance for collecting loose matches (arcsec)");
+      LSST_CONTROL_FIELD(secondMatchCut, double, "max distance for collecting tight matches (arcsec)");
+
+      LSST_CONTROL_FIELD(linMatchMinCount, int, "minimum number of matches to consider a match successfull");
+
+      LSST_CONTROL_FIELD(distortionDegree, int, "polynomial degree used to describe distortions");
+
+      LSST_CONTROL_FIELD(minMatchPerChip, int, "min number of matches per chip");
+
 
         JointcalControl() :
-            sourceFluxField("base_CircularApertureFlux_7")
-        {
+      sourceFluxField("base_CircularApertureFlux_17_0"),
+	linMatchCut(1.5), secondMatchCut(1.), 
+	linMatchMinCount(10), 
+	distortionDegree(3), 
+	minMatchPerChip(5)
+      {
             validate();
         }
         
