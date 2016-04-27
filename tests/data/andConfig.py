d495d833c22a1181c831836b387a217ed9b29c5e
John K. Parejko <parejkoj@uw.edu>
2016-04-27 16:21:50 -0700

    Added Twinkles data files for testing

---
+++ tests/data/andConfig.py
@@ -0,0 +1,7 @@
+root.starGalaxyColumn = "starnotgal"
+root.variableColumn = "isvariable"
+filters = ('u', 'g', 'r', 'i', 'z', 'y')
+root.magColumnMap = {'u':'g', 'g':'g', 'r':'r', 'i':'i', 'z':'i', 'y':'i'}
+root.indexFiles = ['index-022916000.fits', 'index-022916001.fits', 'index-022916002.fits',
+'index-022916003.fits',
+'index-022916004.fits']
