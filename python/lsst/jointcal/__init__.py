8bd6b2b3e20ad6e1128e7dceddc69d797812433c
John K. Parejko <parejkoj@uw.edu>
2018-03-08 23:59:32 -0800

    Lift jointcal/jointcalCoadd to base namespace, add version

--- python/lsst/jointcal/__init__.py
+++ python/lsst/jointcal/__init__.py
@@ -5,10 +5,13 @@ from .astrometryModels import *
 from .ccdImage import *
 from .fitter import *
 from .gtransfo import *
+from .jointcal import *
+from .jointcalCoadd import *
 from .jointcalControl import *
 from .mappings import *
 from .photometryMappings import *
 from .photometryModels import *
 from .photometryTransfo import *
 from .projectionHandler import *
+from .version import *
 __path__ = pkgutil.extend_path(__path__, __name__)
