983d7e4aebebe9ee14d47493ce982cbe7b44a59c
John K. Parejko <parejkoj@uw.edu>
2018-10-26 10:17:46 -0500

    Cleanup fitter docstring

--- include/lsst/jointcal/PhotometryFit.h
+++ include/lsst/jointcal/PhotometryFit.h
@@ -25,8 +25,6 @@ public:
      *
      * @param associations The associations catalog to use in the fitter.
      * @param photometryModel The model to build the fitter for.
-     * @param fluxError The systematic error pedestal to apply to measured instFlux errors
-     *                  (as a percent of instFlux).
      */
     PhotometryFit(std::shared_ptr<Associations> associations,
                   std::shared_ptr<PhotometryModel> photometryModel)
