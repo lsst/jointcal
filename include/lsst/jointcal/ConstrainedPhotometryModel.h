6551376c8c5022b4eb78deb9eff9d34c5ab4d4bd
John Parejko <parejkoj@uw.edu>
2020-01-15 13:39:58 -0800

    Remove unneeded `virtual`

--- include/lsst/jointcal/ConstrainedPhotometryModel.h
+++ include/lsst/jointcal/ConstrainedPhotometryModel.h
@@ -89,7 +89,7 @@ public:
                                      Eigen::VectorXd &derivatives) const override;
 
     /// @copydoc PhotometryModel::print
-    virtual void print(std::ostream &out) const override;
+    void print(std::ostream &out) const override;
 
 protected:
     PhotometryMappingBase *findMapping(CcdImage const &ccdImage) const override;
