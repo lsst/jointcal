075bbbd71cba3dd63c86ff997dae987ac3aa601b
John Parejko <parejkoj@uw.edu>
2020-07-07 22:02:11 -0700

    Add script for plotting chi2 values from jointcal logs

---
+++ bin.src/check_jointcal_logged_chi2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# This file is part of jointcal.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+Plot the chi2 and degrees of freedom values logged by one or more jointcal runs.
+"""
+from lsst.jointcal import check_logged_chi2
+
+if __name__ == "__main__":
+    check_logged_chi2.main()
