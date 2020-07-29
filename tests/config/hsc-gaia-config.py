fb152e12429833f1ab29a653ca07f1474525a216
John Parejko <parejkoj@uw.edu>
2020-07-29 00:47:03 -0700

    remove deprecated HSC config file

--- tests/config/hsc-gaia-config.py
+++
@@ -1,8 +0,0 @@
-config.astrometryRefObjLoader.ref_dataset_name = "gaia"
-config.astrometryRefObjLoader.filterMap = {'u': 'phot_g_mean_mag',
-    'g': 'phot_g_mean_mag',
-    'r': 'phot_g_mean_mag',
-    'i': 'phot_g_mean_mag',
-    'z': 'phot_g_mean_mag',
-    'y': 'phot_g_mean_mag',
-}
