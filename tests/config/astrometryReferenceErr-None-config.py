15b3d44ef2e5d74429df4ffa850aa6632451a5a4
John Parejko <parejkoj@uw.edu>
2021-11-10 16:45:14 -0800

    Rework astrometryReferenceErr=None for gen3

--- tests/config/astrometryReferenceErr-None-config.py
+++ tests/config/astrometryReferenceErr-None-config.py
@@ -1,11 +1,14 @@
 # To test that jointcal fails when loading a refcat without coordinate errors
 # and with no "artificial error" suplied.
+config.astrometryReferenceErr = None
+
 # We have to use the sdss refcat, as gaia includes coord errors; this tests
 # how jointcal handles the case where there is no coord error in the  refcat
 # and no `astrometryReferenceErr` is supplied (should get a useful error message).
-config.astrometryRefObjLoader.ref_dataset_name = "sdss-dr9-fink-v5b"
+config.connections.astrometryRefCat = "sdss_dr9_fink_v5b"
 # jointcal overrides the refcat loader default for gaia, so we have to clear it for SDSS
 config.astrometryRefObjLoader.anyFilterMapsToThis = None
-config.astrometryReferenceErr = None
 # This test refcat cannot apply proper motions.
 config.astrometryRefObjLoader.requireProperMotion = False
+# This old SDSS catalog has capitalized filter names, for some reason.
+config.astrometryRefObjLoader.filterMap = {'r': 'R'}
