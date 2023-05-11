082f0dd85c9ee01de23fa24b8c0b84cc0bf406cf
Eli Rykoff <erykoff@stanford.edu>
2023-05-11 14:04:10 -0700

    Add doRequirePrimary to star selector.

--- tests/config/config.py
+++ tests/config/config.py
@@ -2,3 +2,5 @@
 
 # jointcal test metrics were made including unresolved sources.
 config.sourceSelector['science'].doUnresolved = False
+# Test catalogs do not have detect_isPrimary
+config.sourceSelector['science'].doRequirePrimary = False
